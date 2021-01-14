5a1960d1b81c2837dcaf7fdd12b29d2958eaf8c4
Ryan Wilson <ryan.wilson@empiriolabs.io>
2021-01-13 23:48:47 -0500

    initial commit

---
+++ src/ProblemSet8.java
@@ -0,0 +1,42 @@
+public class ProblemSet8 {
+
+    public boolean common(int[] a, int[] b) {
+
+    }
+
+    public int[] middleman(int[] a, int[] b) {
+
+    }
+
+    public int[] bigger(int[] a, int[] b) {
+
+    }
+
+    public int[] doubleMiddle(int[] a, int[] b) {
+
+    }
+
+    public int[] swapMe(int[] a) {
+
+    }
+
+    public int[] threePoint(int[] a) {
+
+    }
+
+    public int headHoncho(int[] a) {
+
+    }
+
+    public boolean tippingPoint(int[] a, int threshold) {
+
+    }
+
+    public String halfway(int[] a) {
+
+    }
+
+    public int[] sequential(int[] a, int[] b) {
+
+    }
+}
